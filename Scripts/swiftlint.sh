4ab13b3e61a8bf971973538ea64aae21052cb0d0
Almaz Ibragimov <almazrafi@gmail.com>
2020-11-28 20:34:02 +0300

    Fixed Carthage builds (#13)

--- Scripts/swiftlint.sh
+++ Scripts/swiftlint.sh
@@ -4,7 +4,6 @@ if [[ ! $CI && ! $SKIP_SWIFTLINT ]]; then
   if which swiftlint >/dev/null; then
     swiftlint --no-cache
   else
-    echo "error: SwiftLint does not exist, download it from https://github.com/realm/SwiftLint"
-    exit 1
+    echo "warning: SwiftLint does not exist, download it from https://github.com/realm/SwiftLint"
   fi
 fi
