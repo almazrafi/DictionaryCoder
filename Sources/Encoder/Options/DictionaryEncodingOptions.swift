084f01aaa23bd494b53da1d3c2ac8106035300f7
Almaz Ibragimov <almazrafi@gmail.com>
2022-09-20 10:55:40 +0300

    NSNull encoding (#34)

--- Sources/Encoder/Options/DictionaryEncodingOptions.swift
+++ Sources/Encoder/Options/DictionaryEncodingOptions.swift
@@ -7,5 +7,6 @@ internal struct DictionaryEncodingOptions {
     internal let dateEncodingStrategy: DictionaryDateEncodingStrategy
     internal let dataEncodingStrategy: DictionaryDataEncodingStrategy
     internal let nonConformingFloatEncodingStrategy: DictionaryNonConformingFloatEncodingStrategy
+    internal let nilEncodingStrategy: DictionaryNilEncodingStrategy
     internal let keyEncodingStrategy: DictionaryKeyEncodingStrategy
 }
